d296701db363ddd5869bc7afc2c58b21bdc1b941
BhavayJain <77540919+BhavayJain@users.noreply.github.com>
2021-07-14 20:53:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 song="";
 scoreLeftWrist=0;
 leftWristX=0;
-righttWristX=0;
+rightWristX=0;
+scoreRightWrist=0;
 leftWristY=0;
-righttWristY=0;
+rightWristY=0;
 
 function setup(){
     canvas=createCanvas(600 , 500);
@@ -25,6 +26,37 @@ function draw(){
     image(video , 0 , 0 , 600 , 500 );
     fill("#FF0000");
     stroke("#FF0000");
+
+    if(scoreRightWrist>0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+        if(rightWristY>0 && rightWristY<=100){
+        document.getElementById("speed").innerHTML= " Speed = 0.5x";
+        song.rate(0.5);
+        }
+        else if(rightWristY>100 && rightWristY<=200)
+        {
+            document.getElementById("speed").innnerHTML= " Speed = 1x";
+            song.rate(1);
+        }
+        else if(rightWristY>200 && rightWristY<=300)
+        {
+            document.getElementById("speed").innnerHTML= " Speed = 1.5x";
+            song.rate(1.5);
+        }
+        else if(rightWristY>300 && rightWristY<=400)
+        {
+            document.getElementById("speed").innnerHTML= " Speed = 2x";
+            song.rate(2);
+        }
+        else if(rightWristY>400 && rightWristY<=500)
+        {
+            document.getElementById("speed").innnerHTML= " Speed = 2.5x";
+            song.rate(2.5);
+        }
+
+            
+}
 
     if (scoreLeftWrist>0.2){
         circle(leftWristX,leftWristY,20);
@@ -50,6 +82,8 @@ function play(){
 function gotPoses(results){
     if (results.length>0){
         console.log(results);
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        console.log("scorerightWrist"+scoreRightWrist);
         scoreLeftWrist=results[0].pose.keypoints[9].score;
         console.log("scoreLeftWrist"+scoreLeftWrist);
 
